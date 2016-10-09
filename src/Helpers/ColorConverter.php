d4571eee1ec50661d1b5193bda8e1f821ec2a5e9
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-10-09 04:41:31 +0100

    Adding more converters

--- src/Helpers/ColorConverter.php
+++ src/Helpers/ColorConverter.php
@@ -14,4 +14,38 @@ class ColorConverter
      */
     use Converters\HEXTrait,
         Converters\HSVTrait;
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Convert a HEX color to an HSV array.
+     *
+     * @param  string  $hex
+     *
+     * @return array
+     */
+    public static function hexToHsv($hex)
+    {
+        list($red, $green, $blue) = self::hexToRgb($hex);
+
+        return self::rgbToHsv($red, $green, $blue);
+    }
+
+    /**
+     * Convert an HSV to HEX color.
+     *
+     * @param  float|int  $hue
+     * @param  float|int  $saturation
+     * @param  float|int  $value
+     *
+     * @return array
+     */
+    public static function hsvToHex($hue, $saturation, $value)
+    {
+        list($red, $green, $blue) = self::hsvToRgb($hue, $saturation, $value);
+
+        return self::rgbToHex($red, $green, $blue);
+    }
 }
